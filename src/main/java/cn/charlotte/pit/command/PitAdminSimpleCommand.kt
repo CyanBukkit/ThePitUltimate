c7107e6a1f1f5e69154ed2ae6aa4ca147f2bd990
huanmeng_qwq <1871735932@qq.com>
2024-05-20 10:35:47 +0800

    feat: 添加giveBook指令

--- src/main/java/cn/charlotte/pit/command/PitAdminSimpleCommand.kt
+++ src/main/java/cn/charlotte/pit/command/PitAdminSimpleCommand.kt
@@ -424,4 +424,28 @@ class PitAdminSimpleCommand {
         CDKData.loadAllCDKFromData()
         return "§a已刷新"
     }
+
+    @Execute(name = "giveBook")
+    @Permission("pit.admin")
+    fun giveBook(@Context player: Player) {
+        player.inventory.addItem(
+            ItemBuilder(Material.PAPER)
+                .name("&d附魔卷轴")
+                .deathDrop(false)
+                .canDrop(true)
+                .canSaveToEnderChest(true)
+                .internalName("mythic_reel")
+                .uuid(UUID.randomUUID())
+                .lore(
+                    "",
+                    "&7将&6神话物品&7和&d附魔卷轴&7放入神话之井",
+                    "&7将会为该&6神话物品&7带来一个随机的三级 &d&l稀有! &7附魔",
+                    "",
+                    "&7在神话之井使用"
+                )
+                .shiny()
+                .dontStack()
+                .build()
+        )
+    }
 }
