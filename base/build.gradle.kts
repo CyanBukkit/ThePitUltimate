ab17558226c61972b57ed04c62608ecf5d7200f7
CookedBunny <rabbit0w0@outlook.com>
2025-04-13 11:01:14 +0800

    重置版本

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "me.huanmeng"
-version = /*UUID.randomUUID().toString().substring(0,4);*/"4.0.3"
+version = /*UUID.randomUUID().toString().substring(0,4);*/"4.0.2"
 repositories {
     maven("https://maven.aliyun.com/repository/public/")
     mavenCentral()
